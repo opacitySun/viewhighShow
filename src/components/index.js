18a2573e85af70723394c593731f0e7634f6c426
opacitySun <809340810@qq.com>
2018-06-13 16:47:13 +0800

    v1.0

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 // 引入 ECharts 主模块
 import echarts from 'echarts/lib/echarts';
@@ -12,11 +13,15 @@ import 'echarts/lib/component/title';
 
 class Index extends Component {
     constructor(props) {
-        super(props);
+      super(props);
+      this.state = {
+        testUrl: 'http://www.sunbowei.com:3111',
+        posts: []
+      };
     }
     componentDidMount() {
       // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById('echartsTest'));
+      let myChart = echarts.init(document.getElementById('echartsTest'));
       // 绘制图表
       myChart.setOption({
         title: { text: 'ECharts 入门示例' },
@@ -31,10 +36,25 @@ class Index extends Component {
             data: [5, 20, 36, 10, 10, 20]
         }]
       });
+
+      let params = {params:{'id':1}};
+      axios.get(`${this.state.testUrl}/getMusicList`).then(res => {
+        const posts = res.result;
+        this.setState({ posts });
+      }).catch(err => {
+        console.log(err);
+      });
     }
     render () {
       return (
-        <div id="echartsTest" style={{ width: 400, height: 400 }}>
+        <div>
+          <div id="echartsTest" style={{ width: 400, height: 400 }}>
+          </div>
+          <ul>
+            {this.state.posts.map(post =>
+              <li key={post.id}>{post.name}</li>
+            )}
+          </ul>
         </div>
       );
     }
